1042a95ce5a800385b229fad6961bbb851c12680
Listerinnne <00huiju00@gmail.com>
2025-09-10 22:31:42 +0900

    :puzzle: [Algo] PRGMS_181933 풀이

---
+++ 250910/src/PRGMS_181933_flag에_따라_다른_값_반환하기.java
@@ -0,0 +1,14 @@
+import java.util.Map;
+import java.util.function.BiFunction;
+
+public class PRGMS_181933_flag에_따라_다른_값_반환하기 {
+
+    public int solution(int a, int b, boolean flag) {
+
+        Map<Boolean, BiFunction<Integer, Integer, Integer>> function = Map.of(
+                true, (c, d) -> c + d,
+                false, (c, d) -> c - d
+        );
+        return function.get(flag).apply(a, b);
+    }
+}
