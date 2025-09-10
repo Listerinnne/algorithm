73f22cea5e82f207a62237cd5bb51e3304288fce
Listerinnne <00huiju00@gmail.com>
2025-09-10 22:35:55 +0900

    :white_check_mark:[Algo] PRGMS_181933 풀이

--- 250910/src/PRGMS_181933_flag에_따라_다른_값_반환하기.java
+++ 250910/src/PRGMS_181933_flag에_따라_다른_값_반환하기.java
@@ -4,7 +4,6 @@ import java.util.function.BiFunction;
 public class PRGMS_181933_flag에_따라_다른_값_반환하기 {
 
     public int solution(int a, int b, boolean flag) {
-
         Map<Boolean, BiFunction<Integer, Integer, Integer>> function = Map.of(
                 true, (c, d) -> c + d,
                 false, (c, d) -> c - d
